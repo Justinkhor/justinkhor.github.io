cd5f345e7c848dfb729c44a55738745584185fa8
Justinkhor <khorchengee@gmail.com>
2020-09-15 11:31:58 +0800

    Major Updates

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: [],
+  theme: {
+    extend: {
+      borderRadius: {
+        xl: '1rem',
+        '2xl': '2rem',
+        '3xl': '3rem'
+      }
+    },
+  },
+  variants: {
+    margin: ['responsive', 'hover']
+  },
+  plugins: [],
+}
